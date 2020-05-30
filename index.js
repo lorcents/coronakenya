48b559c5d0d8198be26559b125fbcad7a3d6c6ff
lorcents <iannjoroge120@outlook.com>
2020-05-30 10:58:03 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+document.getElementById("confirmed").innerHTML="1745";
+document.getElementById("confirmedToday").innerHTML="127";
+document.getElementById("recovered").innerHTML="438";
+document.getElementById("recoveredToday").innerHTML="20";
+document.getElementById("deaths").innerHTML="62";
+document.getElementById("deathsToday").innerHTML="4";
+
+
+
+
+var x = document.lastModified;
+document.getElementById("lastUpdated").innerHTML = "last update at " + x;
