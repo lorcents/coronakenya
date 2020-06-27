67421af7f12684f9ebaff87fe79982d24d9d31ea
Ian Njoroge <53985098+lorcents@users.noreply.github.com>
2020-06-27 17:00:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 
-document.getElementById("confirmed").innerHTML="5384";
+document.getElementById("confirmed").innerHTML="5533";
 document.getElementById("confirmedToday").innerHTML="183";
-document.getElementById("recovered").innerHTML="1857";
+document.getElementById("recovered").innerHTML="1905";
 document.getElementById("recoveredToday").innerHTML="41";
-document.getElementById("deaths").innerHTML="132";
+document.getElementById("deaths").innerHTML="137";
 document.getElementById("deathsToday").innerHTML="2";
 
 
