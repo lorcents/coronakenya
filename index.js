f78ea4f0e4d4bd81a4999d8568d50fbcbaad4f33
Ian Njoroge <53985098+lorcents@users.noreply.github.com>
2020-07-03 20:35:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
-document.getElementById("confirmed").innerHTML="5533";
-document.getElementById("confirmedToday").innerHTML="183";
-document.getElementById("recovered").innerHTML="1905";
-document.getElementById("recoveredToday").innerHTML="41";
-document.getElementById("deaths").innerHTML="137";
-document.getElementById("deathsToday").innerHTML="2";
+document.getElementById("confirmed").innerHTML="6941";
+document.getElementById("confirmedToday").innerHTML="268";
+document.getElementById("recovered").innerHTML="2109";
+document.getElementById("recoveredToday").innerHTML="20";
+document.getElementById("deaths").innerHTML="152";
+document.getElementById("deathsToday").innerHTML="3";
 
 
 
